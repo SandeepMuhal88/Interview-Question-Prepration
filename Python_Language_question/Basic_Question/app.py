02743c9ef56e868c41126f38670c78fd80620162
SandeepMuhal88 <sandeepmuhal9982@gmail.com>
2026-05-22 22:08:55 +0530

    Change in code

--- Python_Language_question/Basic_Question/app.py
+++ Python_Language_question/Basic_Question/app.py
@@ -1,2 +1,11 @@
 name = input("Enter your name: ")
 print("Hello, " + name + "!")
+
+print("Hello, {}!".format(name))
+
+age = int(input("Enter your age: "))
+print("You are, " + str(age) + " years old.")
+
+# age= input("Enter your age: ")
+# print("You are , "+age+" years old.")
+print("You are, {} years old.".format(age))
--- Python_Language_question/Basic_Question/question_02.py
+++ Python_Language_question/Basic_Question/question_02.py
@@ -1,9 +1,29 @@
-import sys
+# def number(n,m=None):
+#     if m is None:
+#         m=(n,n)
+#     return m
 
-# List Comprehension (uses [])
-list_comp = [x**2 for x in range(10000)]
-print(sys.getsizeof(list_comp)) # ~87616 bytes
 
-# Generator Expression (uses ())
-gen_expr = (x**2 for x in range(10000))
-print(sys.getsizeof(gen_expr))  # ~104 bytes
+# print(number(5))
+# print(number(5,6))
+
+# print(number(90,0))
+
+def append_list(cal, my_list=None):
+    if my_list is None:
+        my_list = []
+    my_list.append(cal)
+    return my_list
+
+
+def delete_list(cal, my_list=None):
+    if my_list is None:
+        my_list = []
+    my_list.remove(cal)
+    return my_list
+
+print(append_list(1))
+print(append_list(2))
+print(append_list(3))
+
+
