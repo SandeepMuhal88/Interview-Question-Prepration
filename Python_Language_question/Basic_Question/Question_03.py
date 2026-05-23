b5cf71645e487c8cfaa549f18e244f570dfd42ca
SandeepMuhal88 <sandeepmuhal9982@gmail.com>
2026-05-23 22:31:13 +0530

    Solve Question

--- Python_Language_question/Basic_Question/Question_03.py
+++ Python_Language_question/Basic_Question/Question_03.py
@@ -1,6 +1,26 @@
-l=[]
-for i in range(2000, 3201):
-    if (i%7==0) and (i%5!=0):
-        l.append(str(i))
+# l=[]
+# for i in range(2000, 3201):
+#     if (i%7==0) and (i%5!=0):
+#         l.append(str(i))
 
-print(','.join(l))
+# print(','.join(l))
+
+
+def fact(x):
+    if x == 0:
+        return 1
+    return x * fact(x - 1)
+
+print("Calculate the factorial of a number")
+x=int(input("Enter a number: "))
+print(fact(x))
+
+
+print("Calculate the squares of numbers from 1 to n")
+
+n=int(input("Enter a number: "))
+d=dict()
+for i in range(1,n+1):
+    d[i]=i*i
+
+print(d)
