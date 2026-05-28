5ce046b056c0c17e53659eada7c1656a9def6d3d
SandeepMuhal88 <sandeepmuhal9982@gmail.com>
2026-05-28 21:47:19 +0530

    Update the code

---
+++ Python_Language_question/Basic_Question/question_05.py
@@ -0,0 +1,21 @@
+import math
+c=50
+h=30
+value = []
+items=[x for x in input().split(',')]
+for d in items:
+    value.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
+
+print(','.join(value))
+
+input_str = input()
+dimensions=[int(x) for x in input_str.split(',')]
+rowNum=dimensions[0]
+colNum=dimensions[1]
+multilist = [[0 for col in range(colNum)] for row in range(rowNum)]
+
+for row in range(rowNum):
+    for col in range(colNum):
+        multilist[row][col]= row*col
+
+print(multilist)
