b226f304b183f4f39f89dd138cc4831550eaa4d4
SandeepMuhal88 <sandeepmuhal9982@gmail.com>
2026-05-30 23:01:24 +0530

    Update the code

---
+++ Python_Language_question/Basic_Question/Question.py
@@ -0,0 +1,27 @@
+# print("The Program of that to calculate Percentage on Any Number:- ")
+
+# num1 = float(input("Enter the number: "))
+
+# num2 = float(input("Enter the percentage(%): "))
+
+# result = num1 * num2 / 100
+
+
+# print(f"{num2}% of {num1} is: {result}")
+
+
+
+# The Program of that to calculate Percentage on Any Number:- 
+# Enter the number: 48
+# Enter the percentage(%): 12
+# 12.0% of 48.0 is: 5.76
+
+
+
+
+a=100
+b=10
+
+c=a/b*2
+print(c)
+
