cf8e7e76f3755f38a5efafa2ec0f782340176a27
SandeepMuhal88 <sandeepmuhal9982@gmail.com>
2026-05-31 21:51:38 +0530

    Update the code

--- Python_Language_question/Basic_Question/Question_06.py
+++ Python_Language_question/Basic_Question/Question_06.py
@@ -1,11 +1,21 @@
-print("Enter the number of rows and columns (separated by a comma):")
+# print("Enter the number of rows and columns (separated by a comma):")
 
-a= int(input("Enter the number of rows: "))
-b=int(input("Enter the number of columns: "))
+# a= int(input("Enter the number of rows: "))
+# b=int(input("Enter the number of columns: "))
 
-multilist = [[0 for col in range(b)] for row in range(a)]
-for row in range(a):
-    for col in range(b):
-        multilist[row][col]= row*col
-print(multilist)
+# multilist = [[0 for col in range(b)] for row in range(a)]
+# for row in range(a):
+#     for col in range(b):
+#         multilist[row][col]= row*col
+# print(multilist)
 
+lines = []
+while True:
+    s = input()
+    if s:
+        lines.append(s.upper())
+    else:
+        break;
+
+for sentence in lines:
+    print(sentence)
